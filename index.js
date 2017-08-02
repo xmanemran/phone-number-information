5eaf1b642ebe17941c4d263f181176deaab93ffa
Emran <xmanemran@gmail.com>
2017-08-02 14:15:14 +0600

    Get operators info

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 var db = require('./db.js').db;
-module.exports = {
-    getCountryInfo : function(number){
-        return db.reduce((obj, value, index)=>{
-          if(number.indexOf(value.code) === 0 || number.indexOf(value.code) === 1)
-            return Number(obj.code) > Number(value.code) ? obj : value;
-          return obj;
-        }, {});
-    }
+
+function checkCountryCode(number, value){
+	return ((number[0] === "+" || number[0] === "(") && number.indexOf(value.code) === 1) || number.indexOf(value.code) === 0 || (number[0] === "0" && number[1] === "0") && number.indexOf(value.code) === 2;
 }
+
+function PhoneInfo(number){
+	return db.reduce(function(obj, value, index){
+	    if(checkCountryCode(number, value)){
+	    	var returnObj = Number(obj.code) > Number(value.code) ? obj : value;
+	    	returnObj.operator = value.operators.filter(function(data){
+	    		if(number.indexOf(data.code) === (number.indexOf(value.code) + value.code.length)){
+	    			data.isValid = number.length - (number.indexOf(value.code) + value.code.length) == data.isValid;
+	    			return true;	
+	    		}
+	    		return false;
+	    	})[0];
+	    	delete value.operators;
+			return returnObj;	
+		}
+		return obj;
+	}, {});
+}
+
+module.exports = PhoneInfo;
